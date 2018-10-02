40074250d1495bab9fe905a679b71eab6669c372
zxpeter <408796230@qq.com>
2018-10-02 23:49:49 +0800

    Update 39_IsBalanced_Solution.py

--- 39_IsBalanced_Solution.py
+++ 39_IsBalanced_Solution.py
@@ -18,3 +18,28 @@ if __name__ == '__main__':
     sol = Solution()
     sol.IsBalanced_Solution()
 
+    
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def isBalanced(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        def check(root):
+            if not root:
+                return 0
+            left = check(root.left)
+            right = check(root.right)
+            if left == -1 or right == -1 or abs(right-left) > 1:
+                return -1
+            return 1 + max(left, right)
+        
+        return check(root) != -1
+        
