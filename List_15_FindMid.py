e9d46526efe8c8742809cf5960a5067bd901a59c
zxpeter <408796230@qq.com>
2018-10-04 18:47:25 +0800

    Update List_15_FindMid.py

--- List_15_FindMid.py
+++ List_15_FindMid.py
@@ -28,12 +28,17 @@ class LinkList():
             return False
         p = head
         q = head
-        while q.next and q.next.next:
+        while q and q.next:
             p = p.next
             q = q.next.next
-
         return p.val
 
+    def middleNode(self, head):
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
 
 if __name__ == '__main__':
     # li = [1, 2, 3, 4, 5]
