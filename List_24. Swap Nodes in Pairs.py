a1a8240ecc429f6c85d12754da20130ee7fdaa51
zxpeter <408796230@qq.com>
2018-10-04 16:17:12 +0800

    Update List_24. Swap Nodes in Pairs.py

--- List_24. Swap Nodes in Pairs.py
+++ List_24. Swap Nodes in Pairs.py
@@ -15,5 +15,30 @@ class Solution(object):
         return self.next
         
         
-        
+# 翻转k-group        
+def reverseKGroup(self, head, k):
+    if head is None or k < 2:
+        return head
+    
+    next_head = head
+    for i in range(k - 1):
+        next_head = next_head.next
+        if next_head is None:
+            return head
+    ret = next_head
+    
+    current = head
+    while next_head:
+        tail = current
+        prev = None
+        for i in range(k):
+            if next_head:
+                next_head = next_head.next
+            _next = current.next
+            current.next = prev
+            prev = current
+            current = _next
+        tail.next = next_head or current
+            
+    return ret  
         
