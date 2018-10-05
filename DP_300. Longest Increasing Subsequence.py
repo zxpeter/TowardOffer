e76e9b7705b762b25dccb6d189d458c438f1bc1d
Coding_Monkey <408796230@qq.com>
2018-10-05 17:26:30 +0800

    Create DP_300. Longest Increasing Subsequence.py

---
+++ DP_300. Longest Increasing Subsequence.py
@@ -0,0 +1,58 @@
+# 300. Longest Increasing Subsequence
+
+class Solution(object):
+    def lengthOfLIS(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not nums: return 0
+        dp = [1] * len(nums)
+        
+        for i in range(len(nums)):
+            for j in range(i):
+                if nums[i] > nums[j]:
+                    dp[i] = max(dp[i], dp[j] + 1)
+        
+        return max(dp)
+        
+        
+# 673. Number of Longest Increasing Subsequence
+      
+class Solution(object):
+    def findNumberOfLIS(self, nums):
+        if not nums:
+            return 0
+        dp = [1]*len(nums)
+        count = [1]*len(nums)
+        
+        for i in range (1, len(nums)):
+            for j in range(i):
+                if nums[i] > nums[j]:
+                    if dp[i] == dp[j]:
+                        dp[i] = dp[j]+1
+                        count[i] = count[j]
+                    elif dp[i] == dp[j]+1:
+                        count[i] += count[j]
+                
+        return sum([count[i] for i in range(len(nums)) if dp[i] == max(dp)])
+        
+674. Longest Continuous Increasing Subsequence
+             
+class Solution(object):
+    def findLengthOfLCIS(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        # Time: O(n)
+        # Space: O(1)
+        max_len = i = 0
+        while i < len(nums):
+            curr = 1
+            while i + 1 < len(nums) and nums[i] < nums[i + 1]:
+                curr, i = curr + 1, i + 1
+            max_len = max(max_len, curr)
+            i += 1
+        return max_len        
+        
