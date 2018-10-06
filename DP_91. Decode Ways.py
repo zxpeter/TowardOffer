822295e2dcad0f62872c671164b9d620c5dc041e
Coding_Monkey <408796230@qq.com>
2018-10-06 16:08:50 +0800

    Create DP_91. Decode Ways.py

---
+++ DP_91. Decode Ways.py
@@ -0,0 +1,20 @@
+91. Decode Ways
+
+class Solution:
+    # @param s, a string
+    # @return an integer
+    def numDecodings(self, s):
+        if not s:
+            return 0
+
+        n = len(s)
+        dp = [0] * (n + 1)
+        dp[0] = 1
+        
+        for i in range(1, n + 1):
+            if s[i-1] != '0':
+                dp[i] += dp[i-1]
+            if len(s[i-2:i]) == 2 and '10' <= s[i-2:i] <= '26':
+                dp[i] += dp[i-2]
+
+        return dp[n]
