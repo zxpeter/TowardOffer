be56eca90333e55b1144cb20df8165b1a4a38e74
Coding_Monkey <408796230@qq.com>
2018-10-13 14:14:56 +0800

    Update 51. N-Queens.py

--- 51. N-Queens.py
+++ 51. N-Queens.py
@@ -1,7 +1,5 @@
 horizontal lines (same rows) are handled because we don't even try to put queens into the same row. In every DFS call we choose the next row, and on that row, we are trying to find a place where the queen can be.
-
 So p is a row number, and q is a column number. And as you can see we choose p as:
-
 p = len(queens)
 So it is always new one.
 
