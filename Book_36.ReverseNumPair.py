22f8ad73b47225f738c9fae5d862b5bbcd1d3fdf
Coding_Monkey <408796230@qq.com>
2018-10-15 23:08:44 +0800

    Create Book_36.ReverseNumPair.py

---
+++ Book_36.ReverseNumPair.py
@@ -0,0 +1,72 @@
+315. Count of Smaller Numbers After Self
+class Solution(object):
+    def countSmaller(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        ans = [0] * len(nums)
+        bst = BinarySearchTree()
+        for i in range(len(nums) - 1, -1, -1):
+            ans[i] = bst.insert(nums[i])
+        return ans
+
+class TreeNode(object):
+    def __init__(self, val):
+        self.leftCnt = 0
+        self.val = val
+        self.cnt = 1
+        self.left = None
+        self.right = None
+
+class BinarySearchTree(object):
+    def __init__(self):
+        self.root = None
+
+    def insert(self, val):
+        if self.root is None:
+            self.root = TreeNode(val)
+            return 0
+        root = self.root
+        cnt = 0
+        while root:
+            if val < root.val:
+                root.leftCnt += 1
+                if root.left is None:
+                    root.left = TreeNode(val)
+                    break
+                root = root.left
+            elif val > root.val:
+                cnt += root.leftCnt + root.cnt
+                if root.right is None:
+                    root.right = TreeNode(val)
+                    break
+                root = root.right
+            else:
+                cnt += root.leftCnt
+                root.cnt += 1
+                break
+        return cnt
+        
+        
+解法III 归并排序求逆序数 （Merge Sort）：
+class Solution(object):
+    def countSmaller(self, nums):
+        def sort(enum):
+            half = len(enum) / 2
+            if half:
+                left, right = sort(enum[:half]), sort(enum[half:])
+                m, n = len(left), len(right)
+                i = j = 0
+                while i < m or j < n:
+                    if j == n or i < m and left[i][1] <= right[j][1]:
+                        enum[i+j] = left[i]
+                        smaller[left[i][0]] += j
+                        i += 1
+                    else:
+                        enum[i+j] = right[j]
+                        j += 1
+            return enum
+        smaller = [0] * len(nums)
+        sort(list(enumerate(nums)))
+        return smaller
