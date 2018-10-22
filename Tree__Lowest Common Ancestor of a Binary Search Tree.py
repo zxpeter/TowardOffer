03a3a609a294c36c46d9e87e34d340de67024c34
Coding_Monkey <408796230@qq.com>
2018-10-23 00:27:25 +0800

    Update Tree__Lowest Common Ancestor of a Binary Search Tree.py

--- Tree__Lowest Common Ancestor of a Binary Search Tree.py
+++ Tree__Lowest Common Ancestor of a Binary Search Tree.py
@@ -22,4 +22,21 @@ class Solution:
             return self.lowestCommonAncestor(root.right, p, q)
         else:
             return root
+
+        
+leetcode.236.   normal binary tree
+def lowestCommonAncestor(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+        if root == p or root == q or not root:
+            return root
+        left = self.lowestCommonAncestor(root.left, p, q) 
+        right = self.lowestCommonAncestor(root.right, p, q) 
+        if left and right:
+            return root
+        return left if left else right
             
