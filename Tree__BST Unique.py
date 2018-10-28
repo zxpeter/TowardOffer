dae07e66def6b7117c415d09db6afe0b745d8a2d
Coding_Monkey <408796230@qq.com>
2018-10-28 23:35:13 +0800

    Update Tree__BST Unique.py

--- Tree__BST Unique.py
+++ Tree__BST Unique.py
@@ -11,3 +11,16 @@ class Solution(object):
     # Catalan Number  (2n)!/((n+1)!*n!)  
     def numTrees(self, n):
         return math.factorial(2*n)/(math.factorial(n)*math.factorial(n+1))
+
+    
+22. Generate Parentheses
+Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+class Solution:
+    def generateParenthesis(self, n):
+        res = [[] for i in range(n+1)]
+        res[0].append('')
+        for i in range(n+1):
+            for j in range(i):
+                res[i] += ['(' + x + ')' + y for x in res[j] for y in res[i - j - 1]]
+        return res[n]
+        
